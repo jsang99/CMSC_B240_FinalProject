16b487f6e51690a8b8897a30a885fa09d7403726
jsang99 <60195598+jsang99@users.noreply.github.com>
2022-04-12 13:38:54 -0400

    added test strings

--- main.py
+++ main.py
@@ -1,9 +1,18 @@
+# test strings
+str1 = "int fun0 ( ) {"
+str2 = "int fun1 ( int a ) {"
+str3 = "int fun2 ( int dog , int cat ) {"
+str4 = "int fun3 ( int x , int y , int z ) {"
+
+
 def ParseFunctionHeader(str):
+    # store parsed string components into an array called words?
+    #
 
 
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    ParseFunctionHeader(str)
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
