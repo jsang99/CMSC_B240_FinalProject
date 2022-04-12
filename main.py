358e9f9b6d8bf3c2328c2d6a2065c519e515d38e
jsang99 <60195598+jsang99@users.noreply.github.com>
2022-04-12 14:53:06 -0400

    starting part 1 milestone 2

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+'''
 # test strings
 str1 = "int fun0 ( ) {"
 str2 = "int fun1 ( int a ) {"
@@ -9,11 +10,11 @@ line3 = "int y = 2 ;"
 line4 = "int a = x + 5 ;"
 line5 = "int b = a + y + 8 ;"
 line6 = "int c = a + x , d , e = b ;"
+'''
 
 def ParseFunctionHeader(str):
     argument=str.split("(")[1].split(")")[:-1][0].replace("int","").replace(",","")
     print(argument.split())
-
 def ParseLine(line):
     array=line.split()
     i=1
@@ -25,9 +26,13 @@ def ParseLine(line):
     print(variables)
 
 def CreateSymbolTable():
-
+    f = open("code.txt", "r")
+    print(f.read())
 
 if __name__ == '__main__':
+    CreateSymbolTable()
+
+    '''
     ParseFunctionHeader(str1)
     ParseFunctionHeader(str2)
     ParseFunctionHeader(str3)
@@ -38,3 +43,5 @@ if __name__ == '__main__':
     ParseLine(line4)
     ParseLine(line5)
     ParseLine(line6)
+    '''
+
