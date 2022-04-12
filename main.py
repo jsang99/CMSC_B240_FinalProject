04027ef0e4412dbda50d8ac7ae15844617361543
jsang99 <60195598+jsang99@users.noreply.github.com>
2022-04-12 14:48:04 -0400

    starting part 1 milestone 2

--- main.py
+++ main.py
@@ -10,20 +10,28 @@ line4 = "int a = x + 5 ;"
 line5 = "int b = a + y + 8 ;"
 line6 = "int c = a + x , d , e = b ;"
 
-
 def ParseFunctionHeader(str):
     argument=str.split("(")[1].split(")")[:-1][0].replace("int","").replace(",","")
     print(argument.split())
 
 def ParseLine(line):
-    print(line.split(","))
+    array=line.split()
+    i=1
+    variables=[]
+    while i< len(array)-1:
+        if array[i-1]=='int' or array[i-1]==',':
+            variables.append(array[i])
+        i+=1
+    print(variables)
+
+def CreateSymbolTable():
 
 
 if __name__ == '__main__':
-    #ParseFunctionHeader(str1)
-    #ParseFunctionHeader(str2)
-    #ParseFunctionHeader(str3)
-    #ParseFunctionHeader(str4)
+    ParseFunctionHeader(str1)
+    ParseFunctionHeader(str2)
+    ParseFunctionHeader(str3)
+    ParseFunctionHeader(str4)
     ParseLine(line1)
     ParseLine(line2)
     ParseLine(line3)
